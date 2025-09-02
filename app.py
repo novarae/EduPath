95cd1ee7779fc5f87a01b147d7837b64e59dc0d1
Chelsea Mumbi <mumbichelsea00@gmail.com>
2025-09-01 23:15:32 -0800

    Updated procfile and app.py

--- app.py
+++ app.py
@@ -285,4 +285,5 @@ def resources():
 # ---------------- Run ----------------
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
